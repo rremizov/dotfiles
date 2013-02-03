5a802b42eb5ebdd09902151d8cbf2e35861f6c77
ceaseandrepent <ceaseandrepent@gmail.com>
2013-02-03 20:37:42 +0600

    Installed dircolors for solarized xterm.

---
+++ links.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run it only when pwd is repo dir
+ln -s `pwd`/.Xdefaults					~/.Xdefaults
+ln -s `pwd`/.config						~/.config
+ln -s `pwd`/.fonts						~/.fonts
+ln -s `pwd`/.profile					~/.profile
+ln -s `pwd`/.xinitrc					~/.xinitrc
+ln -s `pwd`/XTerm						~/XTerm
+ln -s `pwd`/dircolors.ansi-universal	~/.dircolors.ansi
