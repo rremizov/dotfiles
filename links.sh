b62313a569c5db9d9f3006aa73c58fd6ab47049f
ceaseandrepent <ceaseandrepent@gmail.com>
2013-02-21 23:06:41 +0600

    Modified .gitconfig.

--- links.sh
+++ links.sh
@@ -3,6 +3,7 @@
 ln -s `pwd`/.Xdefaults					~/.Xdefaults
 ln -s `pwd`/.config						~/.config
 ln -s `pwd`/.fonts						~/.fonts
+ln -s `pwd`/.gitconfig					~/.gitconfig
 ln -s `pwd`/.profile					~/.profile
 ln -s `pwd`/.xinitrc					~/.xinitrc
 ln -s `pwd`/XTerm						~/XTerm
