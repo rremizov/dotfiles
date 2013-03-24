e038f2feb811bb59760d6dd85c34514b724f627d
ceaseandrepent <ceaseandrepent@gmail.com>
2013-03-25 00:19:16 +0600

    Changed folder names.

--- links.sh
+++ links.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # run it only when pwd is repo dir
-ln -s `pwd`/.Xdefaults					~/.Xdefaults
-ln -s `pwd`/.config						~/.config
-ln -s `pwd`/.fonts						~/.fonts
-ln -s `pwd`/.gitconfig					~/.gitconfig
-ln -s `pwd`/.mutt-colors-solarized		~/.mutt-colors-solarized
-ln -s `pwd`/.profile					~/.profile
-ln -s `pwd`/.xinitrc					~/.xinitrc
 ln -s `pwd`/XTerm						~/XTerm
+ln -s `pwd`/Xdefaults					~/.Xdefaults
+ln -s `pwd`/config						~/.config
 ln -s `pwd`/dircolors.ansi-universal	~/.dircolors.ansi
+ln -s `pwd`/fonts						~/.fonts
+ln -s `pwd`/gitconfig					~/.gitconfig
 ln -s `pwd`/init-cache.sh				~/.init-cache.sh
+ln -s `pwd`/mutt-colors-solarized		~/.mutt-colors-solarized
+ln -s `pwd`/profile						~/.profile
+ln -s `pwd`/xinitrc						~/.xinitrc
