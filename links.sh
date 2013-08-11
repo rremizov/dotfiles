412516200b0f7ee2e5529a878b78939eec8d06dd
ceaseandrepent <ceaseandrepent@gmail.com>
2013-08-11 08:08:15 +0600

    gitconfig moved.

--- links.sh
+++ links.sh
@@ -5,7 +5,6 @@ ln -s `pwd`/Xdefaults                    ~/.Xdefaults
 ln -s `pwd`/config                       ~/.config
 ln -s `pwd`/dircolors.ansi-universal     ~/.dircolors.ansi
 ln -s `pwd`/fonts                        ~/.fonts
-ln -s `pwd`/gitconfig                    ~/.gitconfig
 ln -s `pwd`/init-cache.sh                ~/.init-cache.sh
 ln -s `pwd`/mutt-colors-solarized        ~/.mutt-colors-solarized
 ln -s `pwd`/profile                      ~/.profile
