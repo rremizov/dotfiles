116a9d999501d8d3e0d8d6f0c870019bf9b605d7
rremizov <rremizov@yandex.ru>
2013-11-04 14:26:26 +0600

    luakit: homepage changed

--- config/luakit/userconf.lua
+++ config/luakit/userconf.lua
@@ -1,4 +1,4 @@
-globals.homepage = "http://www.google.ru"
+globals.homepage = "https://duckduckgo.com"
 downloads.default_dir = os.getenv("HOME") .. "/Downloads"
 search_engines.default = search_engines.google
 require "adblock/adblock"
