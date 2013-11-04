700eb9c71914349bc63e15f1297dcef534215e90
rremizov <rremizov@yandex.ru>
2013-11-04 14:28:42 +0600

    luakit: follow hints changed to chars

--- config/luakit/userconf.lua
+++ config/luakit/userconf.lua
@@ -3,4 +3,6 @@ downloads.default_dir = os.getenv("HOME") .. "/Downloads"
 search_engines.default = search_engines.google
 require "adblock/adblock"
 require "adblock/adblock_chrome"
+local s = follow.styles
+follow.style = s.sort(s.reverse(s.charset("asdfghjkl")))
 
