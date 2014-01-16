160b061e39ca9bf8743aa2579156e1d593a377d5
rremizov <rremizov@yandex.ru>
2014-01-17 01:28:13 +0600

    change background color in luakit userscripts

--- local/share/luakit/scripts/habrahabr.user.js
+++ local/share/luakit/scripts/habrahabr.user.js
@@ -5,7 +5,7 @@
 // @run-at         document-end
 // ==/UserScript==
 
-var custom_css = "<style type='text/css'>* { background-color: #073642 !important; color: #839496 !important; } body { width: 75%; margin-left: auto; margin-right: auto; text-align: justify; } body, div, code { font-size: " + Math.floor(window.screen.availHeight / 30) + "px !important; } code { text-align: left; } </style>";
+var custom_css = "<style type='text/css'>* { background-color: #002b36 !important; color: #839496 !important; } body { width: 75%; margin-left: auto; margin-right: auto; text-align: justify; } body, div, code { font-size: " + Math.floor(window.screen.availHeight / 30) + "px !important; } code { text-align: left; } </style>";
 var body = document.getElementsByTagName('body')[0];
 var content = $('div.content_left').html();
 body.innerHTML = content;
