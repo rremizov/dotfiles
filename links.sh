7094554c94e19ad03468009ed58f6d0ddb5666fd
rremizov <rremizov@yandex.ru>
2014-10-05 19:31:26 +0600

    gpg conf

--- links.sh
+++ links.sh
@@ -6,13 +6,14 @@ ln -s `pwd`/Xdefaults                    ~/.Xdefaults
 ln -s `pwd`/config                       ~/.config
 ln -s `pwd`/dircolors.ansi-universal     ~/.dircolors.ansi
 ln -s `pwd`/fonts                        ~/.fonts
+ln -s `pwd`/gpg.conf                     ~/.gnupg/gpg.conf
 ln -s `pwd`/init-cache.sh                ~/.init-cache.sh
 ln -s `pwd`/local                        ~/.local
 ln -s `pwd`/mutt-colors-solarized        ~/.mutt-colors-solarized
 ln -s `pwd`/profile                      ~/.profile
 ln -s `pwd`/tigrc                        ~/.tigrc
 ln -s `pwd`/tmux.conf                    ~/.tmux.conf
+ln -s `pwd`/tmuxinator/system.yml        ~/.tmuxinator/system.yml
 ln -s `pwd`/xinitrc                      ~/.xinitrc
 ln -s `pwd`/xinitrc                      ~/.xsession
-ln -s `pwd`/tmuxinator/system.yml        ~/.tmuxinator/system.yml
 
