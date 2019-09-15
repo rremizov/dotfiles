bf8112b7bbb058b02e8fd19394caa999265924e0
rremizov <rremizov@yandex.ru>
2019-09-15 18:57:50 +0500

    Convert to Ansible role

--- config/luakit/userconf.lua
+++
@@ -1,8 +0,0 @@
-globals.homepage = "https://duckduckgo.com"
-downloads.default_dir = os.getenv("HOME") .. "/Downloads"
-search_engines.default = search_engines.google
-require "adblock/adblock"
-require "adblock/adblock_chrome"
-local s = follow.styles
-follow.style = s.sort(s.reverse(s.charset("asdfghjkl")))
-
---
+++ templates/home/bashrc_core.sh
@@ -0,0 +1,7 @@
+export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin:~/bin
+export EDITOR=vim
+alias ranger='TERM=xterm-256color ranger'
+alias mutt='TERM=xterm-256color mutt'
+
+GPG_TTY=$(tty)
+export GPG_TTY
